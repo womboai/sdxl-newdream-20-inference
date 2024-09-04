f18e889ff8024d9f3dfb6f78219ec421d5351fee
Ashley Wright <ashley@msrandom.net>
2024-09-04 13:52:34 +0300

    Initial

---
+++ src/main.py
@@ -0,0 +1,39 @@
+from io import BytesIO
+from socket import socket, AF_UNIX, SOCK_STREAM
+from sys import byteorder
+
+from pipelines.models import TextToImageRequest
+
+from pipeline import load_pipeline, infer
+
+SOCKET = "/sandbox/inferences.sock"
+
+
+def main():
+    pipeline = load_pipeline()
+
+    with socket(AF_UNIX, SOCK_STREAM) as inference_socket:
+        inference_socket.bind(SOCKET)
+
+        inference_socket.listen(1)
+        connection, _ = inference_socket.accept()
+
+        with connection:
+            while True:
+                size = int.from_bytes(connection.recv(2), byteorder)
+
+                request = TextToImageRequest.model_validate_json(connection.recv(size).decode("utf-8"))
+
+                image = infer(request, pipeline)
+
+                data = BytesIO()
+                image.save(data, format=image.format)
+
+                packet = data.getvalue()
+
+                connection.send(len(packet).to_bytes(4, byteorder))
+                connection.send(packet)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/pipeline.py
@@ -0,0 +1,29 @@
+from PIL.Image import Image
+from diffusers import StableDiffusionXLPipeline
+from pipelines.models import TextToImageRequest
+from torch import Generator
+
+
+def load_pipeline() -> StableDiffusionXLPipeline:
+    pipeline = StableDiffusionXLPipeline.from_pretrained(
+        "stablediffusionapi/newdream-sdxl-20",
+        revision="4bdd502bca7abd1ea57ee12fba0b0f23052958cc",
+        cache_dir="./models",
+        local_files_only=True,
+    ).to("cuda")
+
+    pipeline(prompt="")
+
+    return pipeline
+
+
+def infer(request: TextToImageRequest, pipeline: StableDiffusionXLPipeline) -> Image:
+    generator = Generator(pipeline.device).manual_seed(request.seed) if request.seed else None
+
+    return pipeline(
+        prompt=request.prompt,
+        negative_prompt=request.negative_prompt,
+        width=request.width,
+        height=request.height,
+        generator=generator,
+    ).images
