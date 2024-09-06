7a19876be0cadeb73e44aad8024b0a85e473435b
Ashley Wright <ashley@msrandom.net>
2024-09-07 02:07:45 +0300

    Load from folder instead of cached repository

--- src/pipeline.py
+++ src/pipeline.py
@@ -7,9 +7,7 @@ from torch import Generator
 
 def load_pipeline() -> StableDiffusionXLPipeline:
     pipeline = StableDiffusionXLPipeline.from_pretrained(
-        "stablediffusionapi/newdream-sdxl-20",
-        revision="4bdd502bca7abd1ea57ee12fba0b0f23052958cc",
-        cache_dir="./models",
+        "./models/newdream-sdxl-20",
         torch_dtype=torch.float16,
         local_files_only=True,
     ).to("cuda")
